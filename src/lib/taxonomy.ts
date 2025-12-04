ac7aa4b12351d379eef561ebfaa6820affbba9da
Yahya J. Aifit <yahya@yja.me>
2025-12-04 22:19:17 +0700

    remove tags taxonomy

--- src/lib/taxonomy.ts
+++
@@ -1,7 +0,0 @@
-import type { PublicArticle, PublicTag } from "../sdk/types.gen";
-
-// Deduplicate tags from articles
-export function deduplicateTags(articles: PublicArticle[]): PublicTag[] {
-  const allTags = articles.flatMap((article) => article.tags);
-  return Array.from(new Map(allTags.map((tag) => [tag.slug, tag])).values());
-}
